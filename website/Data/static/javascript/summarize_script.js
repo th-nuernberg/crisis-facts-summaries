cad920179b80b3afc8972697567bf67f33e7eab8
heiderma79923 <heiderma79923@th-nuernberg.de>
2022-09-02 19:57:46 +0200

    changed Parameters: deleted weight, changed Timespan to actual time and date, added dropdown for function type. Added coresponding CSS "code" for these Reshaped the summarize_script.js. It got to convuluted

--- website/Data/static/javascript/summarize_script.js
+++ website/Data/static/javascript/summarize_script.js
@@ -9,39 +9,18 @@ async function make_a_summary(){
    if(document.getElementById("new_summary") != null){
     document.getElementById("new_summary").remove();
    }
- 
-   let ngramms = document.getElementById("ngram").value;
-   // Check if Parameters have been entered
-   if(!ngramms){ngramms = 2}
+   //Get all Parameters
+   let variables_for_summary = get_parameter_as_json();
 
-   let timespan = document.getElementById("time").value;
-   // Check if Parameters have been entered, else set default parameter
-   if(!timespan){timespan = 0}
-
-   let weight = document.getElementById("weight").value;
-   if(!weight){weight = 0}
-
-   let max_length = document.getElementById("max_length").value;
-   if(!max_length){max_length = 600}
-   
-   let summary_question = document.getElementById("text_area").value;
-   if(!summary_question){summary_question = ""}
-
-   // Make a json with all parameters, to be send to the backend
-   let variables_for_summary = {"ngrams": ngramms, "timespan": timespan, "weight":weight, "max_length": max_length, "question": summary_question};
    let response_json;
 
    // Disable Analyse Button while function is calculating and styling
-   let analyse_button = document.getElementById("calculate_button")
-   analyse_button.style.opacity = 0.5
-   analyse_button.style.cursor = "not-allowed"
-   analyse_button.disabled = true
+   let analyse_button = document.getElementById("calculate_button");
+   let loader = document.getElementById("load");
 
-   //Show loading Cricle
-   let loader = document.getElementById("load")
-   loader.style.animation = "spin 2s linear infinite"
-   loader.style.visibility = "visible"
+   close_button_show_loader(analyse_button,loader);
 
+   //Send Data to Backend and await calculated results
    await fetch("http://127.0.0.1:5000/summarize", 
         {
             method: 'POST',
@@ -68,15 +47,11 @@ async function make_a_summary(){
         }).catch((err) => console.error(err));
 
     //Hide loading circle; Enable Analyse Button when function is finished
-    loader.style.visibility = "hidden"
-    loader.style.animation = ""
-    analyse_button.style.cursor = ""
-    analyse_button.style.opacity = 1
-    analyse_button.disabled = false;
+   open_button_hide_loader(analyse_button,loader);
 
 
    // Show new summary
-   let main_Container = document.getElementById("Summary")
+   let main_Container = document.getElementById("Summary");
 
    let new_summary = document.createElement("p");
    new_summary.setAttribute('id', "new_summary");
@@ -85,7 +60,53 @@ async function make_a_summary(){
 
    new_summary.innerHTML = response_json["sentences"];
 
-
    main_Container.append(new_summary);
+}
+
+function get_parameter_as_json(){
+   let ngramms = document.getElementById("ngram").value;
+   if(!ngramms){ngramms = 2}
+
+   let max_length = document.getElementById("max_length").value;
+   if(!max_length){max_length = 600}
+
+   let date_from = document.getElementById("date_from").value;
+   let time_from = document.getElementById("time_from").value;
+
+   let date_to = document.getElementById("time_from").value;
+   let time_to = document.getElementById("time_from").value;
+
+   let function_type = document.getElementById("type_of_function").value;
+
+   // Check if Parameters have been entered
+   let summary_question = document.getElementById("text_area").value;
+   if(!summary_question){summary_question = ""}
    
+   // Make a json with all parameters, to be send to the backend
+   let variables_for_summary = {"ngrams": ngramms,
+                                "max_length": max_length,
+                                "question": summary_question,
+                                "timespan" : {"from": {"date":date_from, "time":time_from}, "to": {"date": date_to, "time":time_to}},
+                                "function_type": function_type,    
+                            };
+   
+   return variables_for_summary;
+}
+
+function close_button_show_loader(analyse_button,loader){
+   analyse_button.style.opacity = 0.5
+   analyse_button.style.cursor = "not-allowed"
+   analyse_button.disabled = true
+
+   //Show loading Cricle animation
+   loader.style.animation = "spin 2s linear infinite"
+   loader.style.visibility = "visible"
+}
+
+function open_button_hide_loader(analyse_button,loader){
+   loader.style.visibility = "hidden"
+   loader.style.animation = ""
+   analyse_button.style.cursor = ""
+   analyse_button.style.opacity = 1
+   analyse_button.disabled = false;
 }
