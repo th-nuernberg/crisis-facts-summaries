4138bcd66903ea4fd57a8e79b639af20ff9ffe7b
heiderma79923 <heiderma79923@th-nuernberg.de>
2022-09-06 17:56:38 +0200

    added new endpoint to choose datasets, At the moment not Operable because of relative Path issues

--- website/Data/app.py
+++ website/Data/app.py
@@ -1,5 +1,6 @@
 #FLASK and FLASK-Endpoint
 import json
+import os
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from glpk_test import *
@@ -21,6 +22,19 @@ def start_summarize():
    summary_as_json = gesamt(ngamms=int(parameters["ngrams"]),max_length=int(parameters["max_length"]))
    print(summary_as_json) # Zum debuggen
    return summary_as_json
+
+
+@app.route("/datasets", methods=["GET"])
+def get_datasets():
+    #get datasetnames as json
+    
+    #momentan nicht korrekter Pfad, weil nicht genau klar welche Datensätze jetzt genutzt werden sollen
+    #relative Pfade funktionieren noch nicht richtig -> Überarbeiten
+    path = "./website/Datensaetze/prepared"
+    dir_list = os.listdir(path)
+    dir_list_json = {"files": dir_list}
+
+    return json.dumps(dir_list_json)
 
 #Run the app:
 if __name__ == "__main__":
