1ca18f271e81e61464fe06302cb64939541baff1
titzda80669 <titzda80669@th-nuernberg.de>
2022-09-27 16:13:29 +0200

    moved mychart.js in summarizescript

--- website/Data/static/javascript/summarize_script.js
+++ website/Data/static/javascript/summarize_script.js
@@ -1,5 +1,20 @@
 // Initial Assignement for Button analyse
 function initAssignment(){
+    var mychartObject = document.getElementById('myChart')
+
+    var chart = new Chart(mychartObject, {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+            datasets: [{
+                label: "Datensatz Nummer 1",
+                backgroundColor: 'rgba(65,105,225,1)',
+                borderColor: 'rgba(65,105,225,1)',
+                data: [3,7,5,2,7,8,6,4]
+            }]
+        }
+    });
+
     let button = document.getElementById("calculate_button");
     button.addEventListener("click",make_a_summary)
 
